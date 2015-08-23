bde4a95cc87fe1b1928bcc6e9658567a40e9f848
gaopan <gaopan461@gmail.com>
2015-08-23 23:49:43 +0800

    @add RandomMapDemo

---
+++ RandomMapDemo/src/org/gaopan/randommap/TestPathFinderGui.java
@@ -0,0 +1,322 @@
+package org.gaopan.randommap;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Point;
+import java.awt.Polygon;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+
+import pathFinding.AStar;
+import pathFinding.AStarMap;
+import pathFinding.PathFinder;
+import pathFinding.heuristics.AStarHeuristic;
+import pathFinding.heuristics.DiagonalHeuristic;
+import pathFinding.utils.Logger;
+import pathFinding.utils.StopWatch;
+
+public class TestPathFinderGui extends JFrame {
+	private enum DrawType {
+		NONE,	// 不需要画
+		DRAWING,// 正在画
+		DRAWED,	// 已画完
+	}
+	
+	private static int startX = 50;
+	private static int startY = 12;
+	private static int goalX = 158;
+	private static int goalY = 110;
+	private static int cellSize = 1;
+	
+	private AStarPanel panel;
+	private JLabel statusBar = new JLabel();
+	
+	private AStarMap map;
+	
+	public TestPathFinderGui() {
+		init();
+		add(panel, BorderLayout.CENTER);
+		add(statusBar, BorderLayout.SOUTH);
+	}
+	
+	private void init() {
+		String actualName = "images/100.txt";
+		File file = new File(actualName);
+		if(!file.exists()) {
+			throw new RuntimeException("file not exist:" + actualName);
+		}
+		
+        try {
+            FileReader freader = new FileReader(file);
+            BufferedReader breader = new BufferedReader(freader);
+            String line = breader.readLine();
+            if(line == null) {
+            	throw new RuntimeException("file empty:" + actualName);
+            }
+            
+            String[] values = line.split(",");
+            if(values.length < 2) {
+            	throw new RuntimeException("file invalid:" + actualName);
+            }
+            
+            int width = Integer.parseInt(values[0]);
+            int height = Integer.parseInt(values[1]);
+            if(width <= 0 || height <= 0) {
+            	throw new RuntimeException("file invalid:" + actualName);
+            }
+            
+            int[][] astar = new int[height][width];
+            for(int h = 0; h < height; ++h) {
+            	line = breader.readLine();
+            	if(line == null) {
+            		throw new RuntimeException("file invalid:" + actualName);
+            	}
+            	
+            	values = line.split(",");
+                if(values.length < width) {
+                	throw new RuntimeException("file invalid:" + actualName);
+                }
+                
+                for(int w = 0; w < width; ++w) {
+                	int obstacle = Integer.parseInt(values[w]);
+                	astar[h][w] = (obstacle == 0) ? 0 : 1;
+                }
+            }
+            
+            panel = new AStarPanel(astar, width, height, cellSize);
+            
+            map = new AStarMap(width, height, astar);
+           
+            breader.close();
+            freader.close();
+        } catch (FileNotFoundException ex) {
+            // TODO Auto-generated catch block
+        	ex.printStackTrace();
+        } catch (IOException ex) {
+            // TODO Auto-generated catch block
+        	ex.printStackTrace();
+        }
+	}
+	
+	public static void main(String[] args) {
+		TestPathFinderGui frame = new TestPathFinderGui();
+		frame.setTitle("TestAStarGui");
+		frame.setSize(frame.panel.mapWidth * cellSize + 100, frame.panel.mapHeight * cellSize + 100);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
+		
+		frame.findPath();
+	}
+	
+	private void findPath() {
+		StringBuffer sb = new StringBuffer();
+		sb.append("start:(").append(startX).append(",").append(startY).append(") ")
+		.append("goal:(").append(goalX).append(",").append(goalY).append(")");
+		
+		Logger log = new Logger();
+		StopWatch s = new StopWatch();
+		
+		log.addToLog("Heuristic initializing...");
+		//AStarHeuristic heuristic = new ClosestHeuristic();
+		AStarHeuristic heuristic = new DiagonalHeuristic();
+		
+		log.addToLog("AStar initializing...");
+		AStar aStar = new AStar(map, heuristic);
+		
+		log.addToLog("Calculating shortest path...");
+		s.start();
+		ArrayList<Point> shortestPath = aStar.calcShortestPath(startX, startY, goalX, goalY);
+		s.stop();
+		
+		log.addToLog("Time to calculate path in milliseconds: " + s.getElapsedTime());
+		sb.append(", Time to calculate path:").append(s.getElapsedTime());
+		
+		log.addToLog("Calculating optimized waypoints...");
+		PathFinder pathfinder = new PathFinder(map);
+		s.start();
+		ArrayList<Point> optimizedPath = pathfinder.calcStraightPath(shortestPath);
+		s.stop();
+		log.addToLog("Time to calculate waypoints: " + s.getElapsedTime() + " ms");
+		sb.append(", Time to calculate waypoints:").append(s.getElapsedTime());
+		
+		statusBar.setText(sb.toString());
+		panel.setPath(shortestPath, optimizedPath);
+	}
+	
+	//-----------------------------------------------------------------------------------------------
+	
+	class AStarPanel extends JPanel {
+		private int mapWidth;
+		private int mapHeight;
+		private int[][] map;
+		private int cellSize;
+		
+		private ArrayList<Point> shortestPath = new ArrayList<Point>();
+		private int shortestPathDrawIndex = 0;
+		private DrawType shortestPathDrawType = DrawType.NONE;
+		
+		private ArrayList<Point> optimizedPath = new ArrayList<Point>();
+		private int optimizedPathDrawIndex = 0;
+		private DrawType optimizedPathDrawType = DrawType.NONE;
+		
+		private Timer drawTimer = new Timer(50, new DrawTimerListener());
+		
+		public AStarPanel(int[][] map, int mapWidth, int mapHeight, int cellSize) {
+			super();
+			this.map = map;
+			this.mapWidth = mapWidth;
+			this.mapHeight = mapHeight;
+			this.cellSize = cellSize;
+			
+			addMouseListener(new MouseAdapter() {
+
+				@Override
+				public void mouseClicked(MouseEvent e) {
+					// TODO Auto-generated method stub
+					super.mouseClicked(e);
+					
+					startX = goalX;
+					startY = goalY;
+					
+					goalX = e.getX() / cellSize;
+					goalY = e.getY() / cellSize;
+					
+					TestPathFinderGui.this.findPath();
+				}
+			});
+		}
+
+		@Override
+		protected void paintComponent(Graphics g) {
+			super.paintComponent(g);
+			
+			for(int h = 0; h < mapHeight; ++h) {
+				int y = h * cellSize;
+				for(int w = 0; w < mapWidth; ++w) {
+					int x = w * cellSize;
+					boolean isObstacle = (map[h][w] == 1 ? true : false);
+					if(isObstacle) {
+						g.setColor(Color.BLACK);
+						g.fillRect(x, y, cellSize, cellSize);
+					} else {
+						g.setColor(Color.GRAY);
+						g.drawRect(x, y, cellSize, cellSize);
+					}
+				}
+			}
+			
+			g.setColor(Color.RED);
+			switch(shortestPathDrawType) {
+			case NONE:
+				break;
+			case DRAWING:
+				shortestPathDrawIndex++;
+				drawShortestPath(g, shortestPathDrawIndex);
+				if(shortestPathDrawIndex == shortestPath.size()) {
+					shortestPathDrawType = DrawType.DRAWED;
+				}
+				break;
+			case DRAWED:
+				drawShortestPath(g, shortestPath.size());
+				break;
+			}
+			
+			if(shortestPathDrawType == DrawType.DRAWING) {
+				return;
+			}
+			
+			g.setColor(Color.BLUE);
+			switch(optimizedPathDrawType) {
+			case NONE:
+				break;
+			case DRAWING:
+				optimizedPathDrawIndex++;
+				drawOptimizedPath(g, optimizedPathDrawIndex);
+				if(optimizedPathDrawIndex == optimizedPath.size()) {
+					optimizedPathDrawType = DrawType.DRAWED;
+				}
+				break;
+			case DRAWED:
+				drawOptimizedPath(g, optimizedPath.size());
+				break;
+			}
+			
+			if(shortestPathDrawType != DrawType.DRAWING && optimizedPathDrawType != DrawType.DRAWING) {
+				if(drawTimer.isRunning()) {
+					drawTimer.stop();
+				}
+			}
+			
+		}
+		
+		private void drawShortestPath(Graphics g, int size) {
+			for(int i = 0; i < size; ++i) {
+				Point point = shortestPath.get(i);
+				int x = point.x * cellSize;
+				int y = point.y * cellSize;
+				g.fillRect(x, y, cellSize, cellSize);
+			}
+		}
+		
+		private void drawOptimizedPath(Graphics g, int size) {
+			Polygon polygon = new Polygon();
+			for(int i = 0; i < size; ++i) {
+				Point point = optimizedPath.get(i);
+				int x = point.x * cellSize + cellSize / 2;
+				int y = point.y * cellSize + cellSize / 2;
+				polygon.addPoint(x, y);
+			}
+			g.drawPolyline(polygon.xpoints, polygon.ypoints, polygon.npoints);
+		}
+
+		public void setPath(ArrayList<Point> shortestPath, ArrayList<Point> optimizedPath) {
+			this.shortestPath = shortestPath;
+			this.shortestPathDrawIndex = 0;
+			this.optimizedPath = optimizedPath;
+			this.optimizedPathDrawIndex = 0;
+			
+			if(shortestPath != null && shortestPath.size() > 0) {
+				this.shortestPathDrawType = DrawType.DRAWING;
+			} else {
+				this.shortestPathDrawType = DrawType.NONE;
+			}
+			
+			if(optimizedPath != null && optimizedPath.size() > 0) {
+				this.optimizedPathDrawType = DrawType.DRAWING;
+			} else {
+				this.optimizedPathDrawType = DrawType.NONE;
+			}
+			
+			if(this.shortestPathDrawType == DrawType.DRAWING
+					|| this.optimizedPathDrawType == DrawType.DRAWING) {
+				drawTimer.start();
+			} else {
+				repaint();
+			}
+		}
+		
+		class DrawTimerListener implements ActionListener {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				repaint();
+			}
+			
+		}
+	}
+}
---
+++ RandomMapDemo/src/org/gaopan/randommap/TestRandomMap.java
@@ -0,0 +1,255 @@
+package org.gaopan.randommap;
+
+import java.awt.FileDialog;
+import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import xtile.Tile;
+import xtile.TileMap;
+
+public class TestRandomMap extends JFrame {    
+	private static final int MAX_ROW = 5;
+	private static final int MAX_COL = 5;
+	private static final int MAX_TRY = 100;
+	private static final int IMAGE_COUNT = 16;
+	
+	private static final String RESOURCE_PATH = "images" + File.separatorChar;
+	
+	private int tileImageWidth;
+	private int tileImageHeight;
+	private int tileAstarWidth;
+	private int tileAstarHeight;
+	
+	private TileMap tileMap;
+	private int[][] obstacleMap;
+	
+	private JButton buttonRandom = new JButton("random");
+	private JLabel statusBar = new JLabel("Cost:");
+	private MapPanel mapPanel;
+	
+	private JButton buttonSaveAstar = new JButton("saveAstar");
+	private FileDialog fdSave;
+	
+	private Map<Integer, List<TileData>> id2Tiles = new HashMap<Integer, List<TileData>>();
+	
+	public TestRandomMap() {
+		init();
+		
+		fdSave = new FileDialog(this, "Save", FileDialog.SAVE);
+		fdSave.addWindowListener(new WindowAdapter() {
+
+			@Override
+			public void windowClosing(WindowEvent arg0) {
+				fdSave.setVisible(false);
+				super.windowClosing(arg0);
+			}
+		});
+		
+		JPanel p1 = new JPanel();
+		p1.add(buttonRandom);
+		p1.add(buttonSaveAstar);
+		
+		buttonRandom.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				onButtonAllClicked();
+			}
+		});
+		
+		buttonSaveAstar.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				fdSave.setVisible(true);
+				
+				String fileName = fdSave.getFile();
+				if(fileName != null)
+                {
+					TestRandomMap.this.saveData();
+                }
+			}
+		});
+		
+		buttonSaveAstar.addMouseListener(new MouseAdapter() {
+
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				super.mouseClicked(e);
+			}
+		});
+		
+		mapPanel = new MapPanel(this, tileImageWidth / tileAstarWidth);
+		onButtonAllClicked();
+		
+		setLayout(null);
+		add(p1);
+		add(mapPanel);
+		add(statusBar);
+		
+		p1.setBounds(0, 0, MAX_COL * tileImageWidth, 50);
+		mapPanel.setBounds(0, 50, MAX_COL * tileImageWidth, MAX_ROW * tileImageHeight);
+		statusBar.setBounds(0, 50 + MAX_ROW * tileImageHeight, MAX_COL * tileImageWidth, 50);
+	}
+	
+	private void init() {
+		// 加载地块图片和寻路数据
+		for(int i = 0; i < IMAGE_COUNT; ++i) {
+			List<TileData> tileList = new ArrayList<>();
+			
+			String fileName = RESOURCE_PATH + i;
+			File file = new File(fileName + TileData.IMAGE_SUFFIX);
+			if(file.exists()) {
+				TileData tileData = new TileData(fileName);
+				tileList.add(tileData);
+			} else {
+				for(int j = 1; j <= 10; ++j) {
+					fileName = RESOURCE_PATH + i + "_" + j;
+					file = new File(fileName + TileData.IMAGE_SUFFIX);
+					if(!file.exists()) {
+						break;
+					}
+					
+					TileData tileData = new TileData(fileName);
+					tileList.add(tileData);
+				}
+			}
+			
+			id2Tiles.put(i, tileList);
+		}
+		
+		TileData tile0 = id2Tiles.get(0).get(0);
+		tileImageWidth = tile0.getImageWidth();
+		tileImageHeight = tile0.getImageHeight();
+		tileAstarWidth = tile0.getAstarWidth();
+		tileAstarHeight = tile0.getAstarHeight();
+		
+		if(tileImageWidth * tileAstarHeight != tileImageHeight * tileAstarWidth) {
+			throw new RuntimeException("image and astar data not match");
+		}
+		
+		// 检测所有地块是否是相同尺寸
+		for(List<TileData> tileList : id2Tiles.values()) {
+			for(TileData tile : tileList) {
+				if(tile.getImageWidth() != tileImageWidth
+						|| tile.getImageHeight() != tileImageHeight
+						|| tile.getAstarWidth() != tileAstarWidth
+						|| tile.getAstarHeight() != tileAstarHeight) {
+					throw new RuntimeException("file size not equal:" + tile.getFileName());
+				}
+			}
+		}
+	}
+	
+	private void onButtonAllClicked() {
+		randomMap();
+		updateMap();
+	}
+	
+	private void randomMap() {
+		long startTime = System.currentTimeMillis();
+		tileMap = new TileMap(MAX_ROW, MAX_COL, MAX_TRY);
+		tileMap.randomGenerate1();
+		tileMap.calcTileCrossType();
+		long costTime = System.currentTimeMillis() - startTime;
+		statusBar.setText("Cost:" + costTime + "ms");
+	}
+	
+	private void updateMap()
+    {
+		int[][] astar = new int[MAX_ROW * tileAstarHeight][MAX_COL * tileAstarWidth];
+		Image[] images = new Image[MAX_ROW * MAX_COL];
+		
+		for(int r = 0; r < MAX_ROW; ++r) {
+			for(int c = 0; c < MAX_COL; ++c) {
+				Tile tile = tileMap.getTile(r, c);
+				int imageId = tile.getCrossMask();
+				List<TileData> tileList = id2Tiles.get(imageId);
+				TileData tileData = null;
+				if(tileList.size() == 1) {
+					tileData = tileList.get(0);
+	        	} else {
+		        	int randIndex = new Random().nextInt(tileList.size());
+		        	tileData = tileList.get(randIndex);
+	        	}
+				
+				images[r * MAX_COL + c] = tileData.getImage();
+				int[][] tileAstar = tileData.getAstar();
+				for(int h = 0; h < tileAstarHeight; ++h) {
+					for(int w = 0; w < tileAstarWidth; ++w) {
+						int astarx = c * tileAstarWidth + w;
+						int astary = r * tileAstarHeight + tileAstarHeight - 1 - h;
+						astar[astary][astarx] = tileAstar[h][w];
+					}
+				}
+			}
+		}
+        
+        mapPanel.setImages(images, MAX_COL, MAX_ROW);
+        mapPanel.setAstar(astar, MAX_COL * tileAstarWidth, MAX_ROW * tileAstarHeight);
+        
+        this.obstacleMap = astar;
+    }
+
+	public static void main(String[] args) {
+		TestRandomMap frame = new TestRandomMap();
+		frame.setTitle("TestRandomMap");
+		frame.setSize(MAX_COL * frame.tileImageWidth + 50, MAX_ROW * frame.tileImageHeight + 150);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
+		frame.setResizable(false);
+	}
+
+	public JLabel getStatusBar() {
+		return statusBar;
+	}
+	
+	private void saveData() {
+		File file = new File(fdSave.getDirectory(),fdSave.getFile());
+        try {
+        	FileWriter fwriter = new FileWriter(file);
+        	BufferedWriter bwriter = new BufferedWriter(fwriter);
+            
+        	int[][] map = this.obstacleMap;
+        	int width = MAX_COL * tileAstarWidth;
+        	int height = MAX_ROW * tileAstarHeight;
+        	
+        	bwriter.write(width + "," + height);
+        	bwriter.newLine();
+        	
+        	for(int h = 0; h < height; ++h) {
+        		for(int w = 0; w < width; ++w) {
+        			bwriter.write(map[h][w] + ",");
+        		}
+        		bwriter.newLine();
+        	}
+        	
+            bwriter.close();  
+            fwriter.close();  
+        } catch (IOException ex) {  
+            // TODO Auto-generated catch block  
+            ex.printStackTrace();  
+        } 
+	}
+
+}
